18bc5f4adb77f7a47d3e7014c126c97497c7d787
dimer <dimer@dim4tech.com>
2022-01-13 11:23:41 -0500

    Fix ALB listener configuration

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -1,29 +1,44 @@
-resource "aws_lb" "prod-uc-mesh-alb" {
-  name               = "prod-uc-mesh-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [ aws_security_group.appmesh-allow-all.id ]
-  subnets            = [ aws_subnet.appmesh-subnet-1.id, aws_subnet.appmesh-subnet-2.id ]
+resource "aws_lb_listener" "uc-mesh-listener-prod" {
+  load_balancer_arn = aws_lb.prod-uc-mesh-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
 
-  enable_deletion_protection = false
+  default_action {
+    type             = "forward"
+    forward {
+        target_group {
+            arn = aws_lb_target_group.uc-blue-mesh-tg.arn
+            weight = 1
+        }
+        target_group {
+            arn = aws_lb_target_group.uc-green-mesh-tg.arn
+            weight = 1
+        }
+        stickiness {
+          duration = 1
+        }
+    }
+  }
 }
 
-resource "aws_lb" "pilot-uc-mesh-alb" {
-  name               = "pilot-uc-mesh-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [ aws_security_group.appmesh-allow-all.id ]
-  subnets            = [ aws_subnet.appmesh-subnet-1.id, aws_subnet.appmesh-subnet-2.id ]
+resource "aws_lb_listener" "uc-mesh-listener-pilot" {
+  load_balancer_arn = aws_lb.pilot-uc-mesh-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
 
-  enable_deletion_protection = false
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.uc-pilot-mesh-tg.arn
+  }
 }
 
-resource "aws_lb" "uc-grafana-alb" {
-  name               = "uc-grafana-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [ aws_security_group.appmesh-allow-all.id ]
-  subnets            = [ aws_subnet.appmesh-subnet-1.id, aws_subnet.appmesh-subnet-2.id ]
+resource "aws_lb_listener" "uc-grafana" {
+  load_balancer_arn = aws_lb.uc-grafana-alb.arn
+  port              = "80"
+  protocol          = "HTTP"
 
-  enable_deletion_protection = false
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.uc-grafana-tg.arn
+  }
 }
